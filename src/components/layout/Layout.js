862067693fda3f1d37fcc27651c0938e01fe782f
Nina Wiik <ninawiik1@outlook.com>
2021-03-02 20:26:16 +0100

    Update Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Home from "../home/Home";
+import InfectionControlForm from "../form/InfectionControlForm";
 
 function Layout() {
   return (
     <>
       <Navigation />
       <Home />
+      <InfectionControlForm />
     </>
   );
 }
